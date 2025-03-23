30edfbfc9ebefdffd0a0b0cae3a7bbfbf1456d8b
RayaheenMseri <rayaheenmseri@gmail.com>
2025-03-23 12:58:06 +0300

    Initial Commit

---
+++ Day14_Assignment/Day14_AssignmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  Day14_AssignmentApp.swift
+//  Day14_Assignment
+//
+//  Created by Rayaheen Mseri on 23/09/1446 AH.
+//
+
+import SwiftUI
+
+@main
+struct Day14_AssignmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
